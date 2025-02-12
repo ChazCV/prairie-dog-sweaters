070b423e4de9da84c629061ff948c5f139e93bea
Chaz Vollmer <chaz.vollmer@senzit.io>
2025-02-11 19:44:37 -0500

    Update base path handling

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Handle GitHub Pages base path
+const basePath = '/prairie-dog-sweaters';
+document.getElementsByTagName('base')[0]?.setAttribute('href', basePath);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
